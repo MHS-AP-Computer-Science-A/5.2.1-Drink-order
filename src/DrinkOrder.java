72741f6fedde1ffa4a9988098d097943b818eb59
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2021-10-26 08:04:25 -0400

    Add files via upload

---
+++ src/DrinkOrder.java
@@ -0,0 +1,16 @@
+public class DrinkOrder
+{
+    //Instance variables go here!
+    
+    //Methods go below!
+    
+    //getSubtotal 
+    //POST: computes and returns the cost of the order before tax
+    
+    //getTax
+    //POST: computes and returns the 6% sales tax on the order
+    
+    //toString
+    //POST: returns a string containing quantity, name, and final order total
+
+}
---
+++ src/DrinkOrderTester.java
@@ -0,0 +1,16 @@
+
+public class DrinkOrderTester {
+    public static void main(String[] args)
+    {
+        //Create DrinkOrder object and set attributes
+        DrinkOrder d = new DrinkOrder();
+        d.name = "Avocado Latte + Sprinkles";
+        d.price = 3.00;
+        d.quantity = 3;
+        
+        System.out.println("Subtotal: "+ d.getSubtotal()); //Displays the order total before tax
+        System.out.println("6% Tax: " + d.getSalesTax()); //Displays the subtotal * .06
+        
+        System.out.println(d);  //Displays the whole order as shown in sample output
+        
+}
